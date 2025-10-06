ad872a7b7f7f45d37e0c7208b6cf3aafd5eabe79
Filip Kjellander <filip.kjellander@gmail.com>
2025-10-06 12:59:15 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        TestClass testClass = new TestClass();
+
+        testClass.TestMethod();
+    }
+
+}
---
+++ src/TestClass.java
@@ -0,0 +1,9 @@
+public class TestClass {
+
+    public String pun = "This is git!";
+
+    public void TestMethod() {
+        System.out.println(pun);
+    }
+
+}
